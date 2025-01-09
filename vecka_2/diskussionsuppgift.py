6c602a2d41efc0ad77f0f0ed40f3c96962c5f669
kanya Suwannajan <kanya.beyon@gmail.com>
2025-01-09 17:22:31 +0100

    strukturerade filerna

---
+++ vecka_2/diskussionsuppgift.py
@@ -0,0 +1,5 @@
+biljettpris = 100  # biljettpris
+pengar_pa_fickan = 200  # pengar på fickan
+print("Det blir " + str(pengar_pa_fickan - biljettpris) + " kronor över.")
+halften = (200 - 100) / 2
+print("Hälften är: " + str(halften))
