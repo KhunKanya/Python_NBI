f974ec8f0779eca97b5c84c26c379b69fc5e11ae
kanya Suwannajan <kanya.beyon@gmail.com>
2025-01-14 19:40:31 +0100

    Rename vars to english and do last exercise 5

--- Veckouppgift2/Exercise_5.py
+++ Veckouppgift2/Exercise_5.py
@@ -1,25 +1,34 @@
 #1 Gör ett program som frågar användaren efter 3 tal.
-tal1 = int(input("Skriv ett heltal: "))
-tal2 = int(input("Skriv ett hektal: "))
-tal3 = int(input("Skriv ett heltal: "))
+number1 = int(input("Skriv ett heltal: "))
+number2 = int(input("Skriv ett hektal: "))
+number3 = int(input("Skriv ett heltal: "))
 
-summa = tal1 + tal2 + tal3
+summa = number1 + number2 + number3
 #2 Programmet ska tala om vilket som är det största talet.
-if tal1 >= tal2 and tal1 >= tal3:
-    maximum = tal1
-elif tal2 >= tal1 and tal2 >= tal3:
-    maximum = tal2
-elif tal3 >= tal1 and tal3 >= tal2:
-    maximum = tal3
+if number1 >= number2 and number1 >= number3:
+    maximum = number1
+elif number2 >= number1 and number2 >= number3:
+    maximum = number2
+elif number3 >= number1 and number3 >= number2:
+    maximum = number3
 else:
     maximum = "n/a"
 
 print(f"Det största talet är {maximum}")
 
 #3 Programmet ska tala om ifall två av talen är lika.
-if tal1 == tal2 and tal1 == tal3:
+if number1 == number2 and number1 == number3:
     print("Alla talen är lika.")
-elif tal1 == tal2 or tal1 == tal3 or tal2 == tal3:
+elif number1 == number2 or number1 == number3 or number2 == number3:
     print("Två av talen är lika.")
 
-
+if number1 != number2 and number1 != number3 and number2 != number3:
+    if (number1 > number2 and number1 < number3) or (number1 < number2 and number1 > number3):
+        middle_num = number1
+    elif (number2 > number1 and number2 < number3) or (number2 < number1 and number2 > number3):
+        middle_num = number2
+    else:
+        middle_num = number3
+    print(f"Det mellersta talet är {middle_num}")
+else:
+    print("Det finns inget mellersta tal i denna uppgift.")
